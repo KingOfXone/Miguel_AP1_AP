64aec201dd3e1fe1374dfe5ddfac657c7b7b366d
Miguel Ángel Betances Beato <miguel_betances1@ucne.edu.do>
2023-10-02 19:40:39 -0400

    Modelo creado

---
+++ Models/Models.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Miguel_AP1_AP.Models
+{
+    public class ModelsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
