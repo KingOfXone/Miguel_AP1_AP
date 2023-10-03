5ac6a037535cf3e31ca8a8105863e295c4089e73
Miguel Ángel Betances Beato <miguel_betances1@ucne.edu.do>
2023-10-02 21:10:26 -0400

    MIgraciones

---
+++ Migrations/20230216173950_Inicial.Designer.cs
@@ -0,0 +1,39 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Miguel_AP1_AP.DAL;
+
+namespace Miguel_AP1_AP.Pages.Migrations
+{
+    [DbContext(typeof(Contexto))]
+    [Migration("20230216173950_Inicial")]
+    partial class Inicial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+#pragma warning disable 612, 618
+            modelBuilder.HasAnnotation("ProductVersion", "7.0.2");
+
+            modelBuilder.Entity("Aportes", b =>
+                {
+                    b.Property<int>("AportesId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER");
+
+                    b.Property<double>("Monto")
+                        .HasColumnType("REAL");
+
+                    b.Property<string>("Persona")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.HasKey("AportesId");
+
+                    b.ToTable("aportes");
+                });
+#pragma warning restore 612, 618
+        }
+    }
+}
---
+++ Migrations/20230216173950_Inicial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Miguel_AP1_AP.Pages.Migrations
+{
+  
+    public partial class Inicial : Migration
+    {
+       
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aportes",
+                columns: table => new
+                {
+                    AportesId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Persona = table.Column<string>(type: "TEXT", nullable: false),
+                    Monto = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aportes", x => x.AportesId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aportes");
+        }
+    }
+}
